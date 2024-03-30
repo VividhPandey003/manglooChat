619c14d9e85a19d6a02c90e5410c4c5ba084d0fa
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-03-30 13:46:18 +0530

    fix
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+const daisyui = require("daisyui");
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
