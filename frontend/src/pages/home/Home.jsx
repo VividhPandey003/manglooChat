2073e7650460c3731ef456ae31e0f15a346eca88
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-04-07 01:38:34 +0530

    Added messages
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import Sidebar from "../../components/sidebar/Sidebar.jsx";
+import MessageContainer from "../../components/messages/MessageContainer.jsx";
 
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg bg-secondary overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20'>
 			<Sidebar />
-			{/* <MessageContainer /> */}
+			<MessageContainer />
 		</div>
 	);
 };
