079080e7e2daab1a08953d32e39989a80b713d60
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-04-09 22:26:51 +0530

    done for today😅
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -10,10 +10,10 @@ const Messages = () => {
   console.log(messages)
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {!loading && messages.length > 0 && messages.map((message) => (
+      {/* {!loading && (messages.length > 0 || messages != undefined ) && messages.map((message) => (
         <Message key={message._id} message={message} />
         
-      ))}
+      ))} */}
       {loading && [...Array(6)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {!loading && (messages.length === 0 || messages == undefined) && (
         <p className='text-center'>Send a message to start the conversation</p>
