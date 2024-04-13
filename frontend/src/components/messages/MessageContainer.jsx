885711cd59f8da96e729310d9f8990e482ccd945
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-04-13 20:23:52 +0530

    minor fix
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,48 +3,45 @@ import useConversation from "../../store/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
-    const { selectedConversation, setSelectedConversation } = useConversation();
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            if (event.key === 'Escape') {
-                setSelectedConversation(null);
-            }
-        };
-        document.addEventListener('keydown', handleKeyDown);
-        return () => {
-            document.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [setSelectedConversation]);
+	const { selectedConversation, setSelectedConversation } = useConversation();
 
-    return (
-        <div className='md:min-w-[450px] flex flex-col'>
-            {!selectedConversation ? (<NoChatSelected />) : (
-                <>
-                    {/* Header */}
-                    <div className='bg-secondary px-4 py-2 mb-2'>
-                        <span className='label-text'></span> To: <span className='text-neutral font-bold'>
-                            {selectedConversation.fullName}</span>
-                    </div>
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
 
-                    <Messages />
-                    <MessageInput />
-                </>
-            )}
-        </div>
-    );
+	return (
+		<div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
+	);
 };
 export default MessageContainer;
 
-export const NoChatSelected = () => {
-    return (
-        <div className='flex items-center justify-center w-full h-full'>
-            <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                <p>Welcome 👋 coderturtle !!</p>
-                <p>Select a chat to start messaging</p>
-                <TiMessages className='text-3xl md:text-6xl text-center' />
-            </div>
-        </div>
-    );
+const NoChatSelected = () => {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='flex items-center justify-center w-full h-full'>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome 👋 {authUser.fullName}!</p>
+				<p>Select a chat to start messaging</p>
+				<TiMessages className='text-3xl md:text-6xl text-center' />
+			</div>
+		</div>
+	);
 };
